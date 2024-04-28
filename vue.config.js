7c40617153a463f7104534313f94b9801fcc69a8
Senriyama <ksinfo1467@gmail.com>
2024-04-28 12:40:13 +0900

    backendでのrunserverまで成功

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  outputDir: '/Users/keis/Desktop/2023/山下研/chat_backend/static',  // Djangoのstaticディレクトリへのパス
+  indexPath: '/Users/keis/Desktop/2023/山下研/chat_backend/templates/index.html',  // Djangoのtemplatesディレクトリへのパス
+  publicPath: process.env.NODE_ENV === 'production' ? '/static/' : '/'
+});
