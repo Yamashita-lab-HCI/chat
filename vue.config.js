3f948377c1b4db4062b2a766869fae183659e5bf
Senriyama <ksinfo1467@gmail.com>
2024-11-24 21:36:18 +0900

    これ以上警告は消せないがnpm run buildができない

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -6,4 +7,12 @@ module.exports = defineConfig({
   indexPath: "/Users/keis/dev/chat_backend/templates/index.html", // Djangoのtemplatesディレクトリへのパス
   publicPath: process.env.NODE_ENV === "production" ? "/static/" : "/",
   filenameHashing: false,
+  productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@ckeditor': path.resolve(__dirname, 'node_modules/@ckeditor'),
+      },
+    },
+  },
 });
